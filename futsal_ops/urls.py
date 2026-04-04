abbd621ea93ab4edaf79762e54e7385366da59ec
Del Ho <del.ho.bh@gmail.com>
2026-04-04 13:38:30 +0800

    Fix homepage redirect name

--- futsal_ops/urls.py
+++ futsal_ops/urls.py
@@ -1,21 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
-from accounts.views import RegisterView, PlayerHomeView, CustomLoginView
 from django.shortcuts import redirect
 
+
+def home_redirect(request):
+    return redirect("player_home")
+
+
 urlpatterns = [
-    path("", home_redirect),
+    path("", home_redirect, name="home"),
     path("admin/", admin.site.urls),
-    path("", include("dashboard.urls")),
     path("events/", include("events.urls")),
     path("registrations/", include("registrations.urls")),
     path("login/", CustomLoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
     path("register/", RegisterView.as_view(), name="register"),
     path("home/", PlayerHomeView.as_view(), name="player_home"),
-    
 ]
-
-def home_redirect(request):
-    return redirect("event_list") # or 'player_home'
