44a39480daf87ebd49112d1e87fa10c059a2f931
Del Ho <del.ho.bh@gmail.com>
2026-04-04 14:39:25 +0800

    updated urls.py

--- futsal_ops/urls.py
+++ futsal_ops/urls.py
@@ -1,4 +1,11 @@
+from django.contrib import admin
+from django.urls import path
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Futsal Ops is LIVE")
+
+urlpatterns = [
+    path("", home, name="home"),
+    path("admin/", admin.site.urls),
+]
