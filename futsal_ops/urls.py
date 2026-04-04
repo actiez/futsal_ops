8a6747965f2f3461a052aafbe931dd16294ef963
Del Ho <del.ho.bh@gmail.com>
2026-04-04 13:44:18 +0800

    Fix login view import

--- futsal_ops/urls.py
+++ futsal_ops/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.contrib.auth import views as auth_views
 
 
 def home_redirect(request):
@@ -12,7 +13,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("events/", include("events.urls")),
     path("registrations/", include("registrations.urls")),
-    path("login/", CustomLoginView.as_view(), name="login"),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
     path("register/", RegisterView.as_view(), name="register"),
     path("home/", PlayerHomeView.as_view(), name="player_home"),
