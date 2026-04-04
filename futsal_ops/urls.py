9fba64826984c00e9179b9bc09967111554e211b
Del Ho <del.ho.bh@gmail.com>
2026-04-04 14:06:21 +0800

    Force root route test

--- futsal_ops/urls.py
+++ futsal_ops/urls.py
@@ -1,23 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.shortcuts import redirect
-from django.contrib.auth import views as auth_views
+from django.urls import path
 from django.http import HttpResponse
 
-def home_redirect(request):
-    return HttpResponse("Futsal Ops is LIVE 🚀")
-
-#def home_redirect(request):
-#    return redirect("player_home")
+def home(request):
+    return HttpResponse("Futsal Ops is LIVE")
 
 urlpatterns = [
-    path("", home_redirect, name="home"),
+    path("", home, name="home"),
     path("admin/", admin.site.urls),
-    path("events/", include("events.urls")),
-    path("registrations/", include("registrations.urls")),
-
-    path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
-
-    # path("register/", RegisterView.as_view(), name="register"),  ❌ disable for now
 ]
