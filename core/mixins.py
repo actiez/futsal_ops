08cc503e20a76e80a2063f64773efc0ed0697084
Del Ho <del.ho.bh@gmail.com>
2026-05-18 12:37:26 +0800

    fixed admin rights

--- core/mixins.py
+++ core/mixins.py
@@ -1,11 +1,12 @@
 from django.shortcuts import redirect
 
+
 class AdminRequiredMixin:
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return redirect("login")
 
-        if not (request.user.is_staff or request.user.is_superuser):
+        if not request.user.is_admin_level():
             return redirect("player_home")
 
         return super().dispatch(request, *args, **kwargs)
